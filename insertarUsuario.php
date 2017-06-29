f96fe9adf69b85976a5f6593ce103d973d59965c
SergioGerman <calleluna.sergiogerman@gmail.com>
2017-06-29 11:11:17 -0800

    Update insertarUsuario.php

--- insertarUsuario.php
+++ insertarUsuario.php
@@ -11,19 +11,19 @@
 
                 <label for="username" >USUARIO</label>
                 <input type="text" name="username" id="username" required="" placeholder="Ingrese el usuario" autocomplete="off">
-
+                <br>
                 <label for="password" >CONTRASEÑA</label>
                 <input type="password" name="password" id="password" required="" placeholder="Ingrese la contraseña">
-
+<br>
                 <label for="cedula" >CEDULA DE IDENTIDAD</label>
                 <input type="text" name="cedula" id="cedula" required="" placeholder="Ingrese su C.I." autocomplete="off">
-
+<br>
                 <label for="administrador">ADMINISTRADOR</label>
                 <input type="radio" name="tipo_usuario" id="administrador" value="1" required="">
-
+<br>
                 <label for="usuario">USUARIO</label>
                 <input type="radio" name="tipo_usuario" id="usuario" value="0" required="">
-
+<br>
 
               <input type="submit" value="REGISTRAR">
             </form>
